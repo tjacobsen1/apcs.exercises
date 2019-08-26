5044d751a03a1892a998600df9676c01e995d07f
Thomas Jacobsen <thomasj4332@gmail.com>
2019-08-26 12:44:44 -0600

    First draft

--- chapter10/Trig.java
+++ chapter10/Trig.java
@@ -2,11 +2,13 @@ public class Trig
 {
     public static void main (String[] args)
     {
-        final double value1 = 0.5236
-        final double 
+        final double value1 = 0.5236;
+        final double value2 = 0.5235;
+        numsquared = value1 * value2;
 
-        Math.sin();
-        Math.cos();
+        System.out.println("Sine of (value1): " + Math.sin(value1));
+        System.out.println("Cosine of (value2):" + Math.cos(value2));
+        System.out.println("Sine of (value1) * cosine of (value2): " + numsquared);
     }
 
 }
