d2aa8e7ef8331d2898a96c118c183f73e411510c
Thomas Jacobsen <thomasj4332@gmail.com>
2019-08-27 12:48:54 -0600

    Finished code

--- chapter11/StringDemo2.java
+++ chapter11/StringDemo2.java
@@ -5,7 +5,7 @@ public class StringDemo2
         String str;
         int len;
         
-        str = new String("Elementary, my dear Watson!");
+        str = new String("Elementary, my dear Watson! joe mama  hahaha");
 
         len = str.length(); // call the length() method of the object
 
