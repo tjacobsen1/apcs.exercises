66f7685023ab814f793ed192864b7c3530cacad5
Thomas Jacobsen <thomasj4332@gmail.com>
2019-08-27 12:46:22 -0600

    Initial commit

---
+++ chapter11/Object.java
@@ -0,0 +1,11 @@
+public class Object 
+{
+    public static void main (String args[])
+    {
+        String str = new String("Hello World!");
+        String str2;
+        System.out.println(str);
+        System.out.println(secondVariable);
+    }
+
+}
