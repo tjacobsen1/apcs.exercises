871e0e0b64900d98d0f2adcb2ba071fced8225f7
Thomas Jacobsen <thomasj4332@gmail.com>
2019-08-28 12:08:57 -0600

    Initial commit

---
+++ chapter11/StringDemo3.java
@@ -0,0 +1,11 @@
+public class StringDemo3
+{
+    public static void main(String[] args)
+    {
+        String str = new String("Gold if a good walk spoiled.");
+
+        String sub = str.substring(9, 18);
+
+        System.out.println(sub);
+    }
+}
---
+++ chapter11/StringExercise.java
@@ -0,0 +1,10 @@
+public class StringExercise
+{
+    public static void main(String[] args)
+    {
+       String first = new String("    In a Hole    in the ground there lived a Hobbit.   ");
+       String second;
+       second = first.trim();
+        System.out.println(second);
+    }
+}
