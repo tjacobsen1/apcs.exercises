cc331ef3fd5dc026b1843ac0230645b32eb7b204
Thomas Jacobsen <thomasj4332@gmail.com>
2019-09-16 13:01:05 -0600

    go heckker yourself

--- chapter19/Sequential.java
+++ chapter19/Sequential.java
@@ -7,9 +7,20 @@ public class Sequential {
         Scanner scan = new Scanner(System.in);
         int n = 0;
         int sum;
+        int end = 0;
+        int x = 0;
 
         System.out.println("Give me N: ");
         n = scan.nextInt();
         
+
+        while (x <= n) {
+            x = x + 1;
+            end = (n*(n+1))/2;
+        }
+        sum = (n*(n+1))/2;
+        
+        System.out.println("Loop sum = " + end);
+        System.out.println("Formula sum = " + sum);
     }
 }
---
+++ chapter19/Words.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Words {
+    public static void main(String[]args) { 
+        
+        Scanner san = new Scanner(System.in);
+        
+    }
+}
