d6dc6608a97b5bfb93c32217a35d21ee7707d6c7
Thomas Jacobsen <thomasj4332@gmail.com>
2019-09-18 09:26:29 -0600

    Worked first period

--- chapter23/Etothex.java
+++ chapter23/Etothex.java
@@ -6,6 +6,10 @@ public class Etothex {
         Scanner scan = new Scanner(System.in);
         int x, term, sum = 0;
 
+
+        System.out.println("Enter X: ");
+        x = scan.nextInt();
+        
         while (term > 1.0E-12) {
             1
         }
