4e9c6cff119b6d7c03e8333affefa9fb4307ab5b
Thomas Jacobsen <thomasj4332@gmail.com>
2019-09-17 21:47:03 -0600

    Initial commit

--- chapter20/HarmonicSum.java
+++ chapter20/HarmonicSum.java
@@ -1,9 +1,22 @@
 import java.util.Scanner;
 
-public class HarmonicSum {
+public class HarmonicSum
+{
+    public static void main(String[]args)
+    {
+        Scanner scan = new Scanner (System.in);
+        double n;
+        double m = 1;
+        double sum = 0;
 
-    public static void main(String[]args) {
+        System.out.println("Enter N: ");
+        n = scan.nextDouble();
 
-        
+        while (m <= n) {
+            sum = 1/m + sum;
+            m = m + 1;
+        }
+
+        System.out.println("Sum is: " + sum);
     }
 }
