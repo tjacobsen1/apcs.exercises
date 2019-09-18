f58f319dbbf625efeac3b0312978b2335f6648cb
Thomas Jacobsen <thomasj4332@gmail.com>
2019-09-17 22:25:39 -0600

    Final code

--- chapter19/Words.java
+++ chapter19/Words.java
@@ -1,9 +1,29 @@
 import java.util.Scanner;
 
-public class Words {
-    public static void main(String[]args) { 
-        
-        Scanner san = new Scanner(System.in);
-        
+public class Words
+{
+    public static void main (String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+
+        String word1, word2;
+        int dots;
+
+        System.out.println("Enter First word:");
+        word1 = scan.nextLine();
+
+        System.out.println("Enter Second word:");
+        word2 = scan.nextLine();
+
+        dots = 30 - (word1.length() + word2.length());
+
+        System.out.print(word1);
+
+        while (dots > 0)
+        {
+            System.out.print(".");
+            dots -= 1;
+        }
+            System.out.print(word2);
     }
 }
