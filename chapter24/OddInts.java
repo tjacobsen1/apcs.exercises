131efac80c771c98136fe54b9d77bdf86b3afbdd
Thomas Jacobsen <thomasj4332@gmail.com>
2019-09-23 12:49:53 -0600

    Initial commit

---
+++ chapter24/OddInts.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class OddInts {
+
+    public static void main(String[]args) {
+        int n, sum;
+        Scanner scan = new Scanner(System.in);
+        System.println("Give me N: ");
+        n = scan.nextInt();
+
+        for (sum = 0; ) {
+
+        }
+    }
+}
