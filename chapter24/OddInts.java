bff28d8e53439adf9f9c7b9041b6c13c45a629c1
Thomas Jacobsen <thomasj4332@gmail.com>
2019-09-24 12:53:07 -0600

    didn't finish

--- chapter24/OddInts.java
+++ chapter24/OddInts.java
@@ -3,13 +3,24 @@ import java.util.Scanner;
 public class OddInts {
 
     public static void main(String[]args) {
-        int n, sum;
+        int N, sum, i, j;
+        int Nsquared, sumsquared;
+        sumsquared =1;
+        sum = 1;
         Scanner scan = new Scanner(System.in);
-        System.println("Give me N: ");
-        n = scan.nextInt();
+        System.out.println("Give me N: ");
+        N = scan.nextInt();
 
-        for (sum = 0; ) {
-
+        for (i = 1; sum < N; ) {
+            i++;
         }
+        System.out.println("Final sum of all odd integers from 1 to " + N + ": " + sum);
+        
+        Nsquared = N * N;
+
+        for (j = 1; sumsquared < Nsquared; ) {
+            j++;
+        }
+        System.out.println("Final sum of " + N + " squared which is " + Nsquared + " is: " + sumsquared);
     }
 }
