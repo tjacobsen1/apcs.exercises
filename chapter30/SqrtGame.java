b67d1e0bc802a7aa2288198dab50f6ba7b98a394
Thomas Jacobsen <thomasj4332@gmail.com>
2019-10-07 12:54:24 -0600

    Not done

--- chapter30/SqrtGame.java
+++ chapter30/SqrtGame.java
@@ -1,40 +1,42 @@
 import java.util.*;
-
 public class SqrtGame {
 
     public static void main(String[]args) {
         Scanner scan = new Scanner(System.in);
         Random rand = new Random();
-        
-        
-        boolean endOfGame = false;
-
+        String player1;
+        String player2;
+        int player1wins = 0;
+        int player2wins = 0;
         int rounds;
         double player1guess, player2guess;
-        int player1wins, player2wins = 0;
 
         System.out.println("How many rounds? ");
         rounds = scan.nextInt();
 
-        while (!endOfGame) {
-            double x = rand.nextDouble(99);
+        System.out.println("First Player, sign in: ");
+        scan.nextLine();
+        player1 = scan.nextLine();
+        System.out.println("Second player, sign in: ");
+        player2 = scan.nextLine();
 
-            System.out.println("First Player, sign in: ");
-            String player1 = scan.nextLine();
-            System.out.println("Second player, sign in: ");
-            String player2 = scan.nextLine();
+        boolean endOfGame = false;
+        while (!endOfGame) {
+            
+            double x = rand.nextDouble()*99;
+            double sqrtx = Math.sqrt(x);
 
             System.out.println("What is the square root of " + x + "?");
             System.out.println(player1 + ", your guess: ");
             player1guess = scan.nextDouble();
-            System.out.println(player2 + ", your guess: ");
+            System.out.println( player2 + ", your guess: ");
+            player2guess = scan.nextDouble();
             System.out.println("The correct square root of " + x + " is " + Math.sqrt(x));
-            System.out.println(player1 + " is " + player1difference + " away and " + player2 + " is " + player2difference + " away. ");
 
-            double player1difference = Math.abs(player1guess - x);
-            double player2difference = Math.abs(player2guess - x);
-            if (player1difference < player2guess) {
-                
+            double player1difference = Math.abs(player1guess - sqrtx);
+            double player2difference = Math.abs(player2guess - sqrtx);
+            System.out.println( player1 + " is " + player1difference + " away and " + player2 + " is " + player2difference + " away. ");
+            if ( player1difference < player2difference ) {
                 System.out.println(player1 + " wins!");
                 rounds-=1;
                 player1wins+=1;
@@ -43,13 +45,11 @@ public class SqrtGame {
                 rounds-=1;
                 player2wins+=1;
             }
-            
-            if (rounds = 0) {
+            if (rounds == 0) {
                 endOfGame = true;
             }
 
         }
-
         System.out.println("---- Final Score ---- ");
         System.out.println(player1 + ": " + player1wins + "   " + player2 + ": " + player2wins);
     }
