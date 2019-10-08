49654d8db6c540abd20ae983b6f61025143e9487
Thomas Jacobsen <thomasj4332@gmail.com>
2019-10-08 12:54:06 -0600

    Worked in class

--- chapter30/DiceGame.java
+++ chapter30/DiceGame.java
@@ -2,7 +2,32 @@ import java.util.*;
 
 public class DiceGame {
     public static void main(String[]args) { 
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
 
+        int playerscore = 0;
+        int compscore = 0;
+
+
+        boolean endOfGame = false;
+
+        while (!endOfGame) {
+            
+
+
+
+
+
+
+
+
+
+
+
+            if (playerscore >= 100 || compscore >= 100) {
+                endOfGame = true;
+            }
+        }
 
         
     }
