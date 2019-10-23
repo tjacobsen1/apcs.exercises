8581d4a6ffb8afd086b5b7f603d7ab787c7edb55
Thomas Jacobsen <thomasj4332@gmail.com>
2019-10-23 12:50:59 -0600

    Done

--- gpa/Car.java
+++ gpa/Car.java
@@ -4,7 +4,46 @@ public class Car {
     private String type;
 
     private int tank;
-    private int time;
+    private double time;
+    private double mpg;
 
-    public Car(String )
+    public Car(String car, String type, int tank, double time) {
+        this.car = car;
+        this.type = type;
+        this.tank = tank;
+        this.time = time;
+    }
+
+    public String getCar() {
+        return this.car;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public int getTank() {
+        return this.tank;
+    }
+
+    public double getTime() {
+        return this.time;
+    }
+
+    public double getMPG() {
+        mpg = (60 * this.time) / (this.tank * 0.264172);
+        return this.mpg;
+    }
+
+    public Boolean getFuelEfficiency() {
+        if (this.type == "Sedan" && mpg >= 50) {
+            return true;
+        } else if (this.type == "SUV" && mpg >= 40) {
+            return true;
+        } else if(this.type == "Truck" && mpg >= 30) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- gpa/Fjord.java
+++ gpa/Fjord.java
@@ -1,12 +1,19 @@
 public class Fjord {
 
     public static void main (String[]args) {
-        Car takk = new Car("Sedan", "50", "8");
-        Car beklager = new Car("Sedan", "40", "7.5");
-        Car vakker = new Car("SUV", "60", "5");
-        Car stygg = new Car("SUV", "55", "6");
-        Car vanskellig = new Car("Truck", "65", "4.75");
-        Car lastebil = new Car("Truck", "70", "5.5");
+        Car car = new Car("Takk", "Sedan", 40, 9.5);
+        Car car2 = new Car("Beklager", "Sedan", 45, 7.5);
+        Car car3 = new Car("Vakker", "SUV", 60, 7.5);
+        Car car4 = new Car("Stygg", "SUV", 50, 9);
+        Car car5 = new Car("Vanskellig", "Truck", 60, 8.75);
+        Car car6 = new Car("Lastebil", "Truck", 70, 5.5);
 
+
+        System.out.println(car.getCar() + " MPG: " + car.getMPG() + " Efficiency: " + car.getFuelEfficiency());
+        System.out.println(car2.getCar() + " MPG: " + car2.getMPG() + " Efficiency: " + car2.getFuelEfficiency());
+        System.out.println(car3.getCar() + " MPG: " + car3.getMPG() + " Efficiency: " + car3.getFuelEfficiency());
+        System.out.println(car4.getCar() + " MPG: " + car4.getMPG() + " Efficiency: " + car4.getFuelEfficiency());
+        System.out.println(car5.getCar() + " MPG: " + car5.getMPG() + " Efficiency: " + car5.getFuelEfficiency());
+        System.out.println(car6.getCar() + " MPG: " + car6.getMPG() + " Efficiency: " + car6.getFuelEfficiency());
     }
 }
