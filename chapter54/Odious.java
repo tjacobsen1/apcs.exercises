c504e2746b55ecf77a8b1196048cd0bde740b9e1
Thomas Jacobsen <thomasj4332@gmail.com>
2019-11-20 13:13:16 -0700

    Worked in class

---
+++ chapter54/Odious.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Odious {
+
+    public static void main(String[]args) {
+        Scanner scan = new Scanner(System.in);
+        int 
+        System.out.println("Input a nonnegative integer: ");
+        num = scan.nextInt();
+        
+        while (num != 0) {
+            
+        }
+    }
+}
