0fbba0dd41e6bd198d6b16ebba2b9939dfddfa87
Thomas Jacobsen <thomasj4332@gmail.com>
2019-12-16 12:38:36 -0700

    Re-did Chapter23 Exercise 4 to get credit back

--- chapter23/Etothex.java
+++ chapter23/Etothex.java
@@ -4,15 +4,33 @@ public class Etothex {
     
     public static void main(String[]args) {
         Scanner scan = new Scanner(System.in);
-        int x, term, sum = 0;
-
-
+        int x = 0;
+        double sum = 0.0;
+        int n = 0;
+        double term = 1;
         System.out.println("Enter X: ");
         x = scan.nextInt();
         
         while (term > 1.0E-12) {
-            sum = sum + term;
+            term = Math.pow(x, n) / factorial(n);
+            sum += term;
+            System.out.println("n:" + n + "\nterm:" + term + "\nsum" + sum);
+            n++;
         }
 
+        System.out.println("Mine: " + sum);
+        System.out.println("Actual: " + Math.exp(x));
+    }
+
+    
+    public static double factorial(int num) {
+        int i = 1;
+        double factorial = 1; 
+
+        while ( i <= num) {
+            factorial *= i;
+            i++;
+        }
+        return factorial;
     }
 }
